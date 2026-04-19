6a0d2208d40e14b9667691fbbeab378b5199e586
rishabhnetcore <rishabh.singh@netcorecloud.com>
2026-04-20 02:01:01 +0530

    ConcatenationOfArray_L1929

---
+++ src/Array/ConcatenationOfArray_L1929.java
@@ -0,0 +1,31 @@
+package Array;
+
+public class ConcatenationOfArray_L1929 {
+
+    public static void main(String[] args) {
+        int[] ans = getConcatenation(new int[] { 1, 2, 1 });
+        for (int i : ans) {
+            System.out.print(i + " ");
+        }
+
+    }
+
+    public static int[] getConcatenation(int[] nums) {
+
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+        System.out.println(ans.length - n);
+        for (int i = 0; i < ans.length - n; i++) {
+            ans[i] = nums[i];
+
+        }
+
+        for (int i = n; i < ans.length; i++) {
+            ans[i] = nums[i - n];
+        }
+
+        return ans;
+
+    }
+
+}
