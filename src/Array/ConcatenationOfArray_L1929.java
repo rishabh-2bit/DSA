1c75cd3560b4a37a743039f5b6587785ba3f6822
rishabhnetcore <rishabh.singh@netcorecloud.com>
2026-04-20 02:09:58 +0530

    optimized solution

--- src/Array/ConcatenationOfArray_L1929.java
+++ src/Array/ConcatenationOfArray_L1929.java
@@ -14,7 +14,7 @@ public class ConcatenationOfArray_L1929 {
 
         int n = nums.length;
         int[] ans = new int[2 * n];
-        System.out.println(ans.length - n);
+        // Brute force
         for (int i = 0; i < ans.length - n; i++) {
             ans[i] = nums[i];
 
@@ -23,7 +23,13 @@ public class ConcatenationOfArray_L1929 {
         for (int i = n; i < ans.length; i++) {
             ans[i] = nums[i - n];
         }
-
+        /*
+         * Optimized code
+         * for (int i = 0; i < n; i++) {
+         * ans[i] = nums[i];
+         * ans[i + n] = nums[i];
+         * }
+         */
         return ans;
 
     }
